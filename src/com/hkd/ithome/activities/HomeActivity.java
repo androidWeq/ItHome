5a4689ec400a05d4bc275aa776d29cb6f718f0a0
WEQ <405793463@qq.com>
2016-09-14 09:29:30 +0800

    First commit.

---
+++ src/com/hkd/ithome/activities/HomeActivity.java
@@ -0,0 +1,174 @@
+package com.hkd.ithome.activities;
+
+import com.example.ithome.R;
+import com.hkd.ithome.fragment.HotgoodsFragment;
+import com.hkd.ithome.fragment.ITcircleFragment;
+import com.hkd.ithome.fragment.MineFragment;
+import com.hkd.ithome.fragment.NewsFragment;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+
+public class HomeActivity extends FragmentActivity {
+
+	@ViewInject(R.id.rgHome)
+	RadioGroup rgHome;
+	@ViewInject(R.id.rbNews)
+	RadioButton rbNews;
+	@ViewInject(R.id.rbHotgoods)
+	RadioButton rbHotgoods;
+	@ViewInject(R.id.rbItcircle)
+	RadioButton rbItcircle;
+	@ViewInject(R.id.rbMine)
+	RadioButton rbMine;
+
+	FragmentManager manager;
+	FragmentTransaction tran;
+
+	NewsFragment newsFragment;
+	HotgoodsFragment hotgoodsFragment;
+	ITcircleFragment iTcircleFragment;
+	MineFragment mineFragment;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+		ViewUtils.inject(this);
+
+		manager = getSupportFragmentManager();
+		newsFragment = new NewsFragment();
+		manager.beginTransaction().add(R.id.fragHome, newsFragment).commit();
+
+		rgHome.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(RadioGroup arg0, int arg1) {
+				// TODO Auto-generated method stub
+				FragmentTransaction ft = manager.beginTransaction();
+				switch (arg1) {
+				case R.id.rbNews:
+					if (newsFragment == null) {
+						newsFragment = new NewsFragment();
+						ft.add(R.id.fragHome, newsFragment);
+					} else {
+						ft.show(newsFragment);
+					}
+
+					if (hotgoodsFragment != null && iTcircleFragment != null
+							&& mineFragment != null) {
+						ft.hide(hotgoodsFragment);
+						ft.hide(iTcircleFragment);
+						ft.hide(mineFragment);
+					}
+					ft.commit();
+
+					break;
+
+				case R.id.rbHotgoods:
+					if (hotgoodsFragment == null) {
+						hotgoodsFragment = new HotgoodsFragment();
+						ft.add(R.id.fragHome, hotgoodsFragment);
+					} else {
+						ft.show(hotgoodsFragment);
+					}
+					
+					if (newsFragment != null && iTcircleFragment != null
+							&& mineFragment != null) {
+						ft.hide(newsFragment);
+						ft.hide(iTcircleFragment);
+						ft.hide(mineFragment);
+					}
+					ft.commit();
+
+					
+					
+					
+					break;
+				case R.id.rbItcircle:
+					
+					if (iTcircleFragment == null) {
+						iTcircleFragment = new ITcircleFragment();
+						ft.add(R.id.fragHome, iTcircleFragment);
+					} else {
+						ft.show(iTcircleFragment);
+					}
+					
+					if (newsFragment != null && hotgoodsFragment != null
+							&& mineFragment != null) {
+						ft.hide(newsFragment);
+						ft.hide(hotgoodsFragment);
+						ft.hide(mineFragment);
+					}
+					ft.commit();
+					
+					
+					break;
+				case R.id.rbMine:
+					
+					if (mineFragment == null) {
+						mineFragment = new MineFragment();
+						ft.add(R.id.fragHome, mineFragment);
+					} else {
+						ft.show(mineFragment);
+					}
+					
+
+					if (newsFragment != null && hotgoodsFragment != null
+							&& iTcircleFragment != null) {
+						ft.hide(newsFragment);
+						ft.hide(hotgoodsFragment);
+						ft.hide(iTcircleFragment);
+					}
+					ft.commit();
+					
+					
+					break;
+
+				default:
+					break;
+				}
+			}
+		});
+		
+
+	}
+	
+	FragmentStatePagerAdapter mAdapter = new FragmentStatePagerAdapter(
+			getSupportFragmentManager()) {
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 4;
+		}
+
+		@Override
+		public Fragment getItem(int arg0) {
+			// TODO Auto-generated method stub
+			switch (arg0) {
+			case 0:
+				return new NewsFragment();
+			case 1:
+				return new HotgoodsFragment();
+			case 2:
+				return new ITcircleFragment();
+			case 3:
+				return new MineFragment();
+			default:
+				return null;
+			}
+		}
+	};
+	
+	
+}
---
+++ src/com/hkd/ithome/adapter/HotGoodsListViewAdapter.java
@@ -0,0 +1,67 @@
+package com.hkd.ithome.adapter;
+
+import java.util.List;
+
+import com.example.ithome.R;
+
+import android.content.Context;
+import android.text.format.Time;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class HotGoodsListViewAdapter extends BaseAdapter {
+	
+	private List<String> datas;
+	private Context context;
+	
+	
+	
+	public HotGoodsListViewAdapter(List<String> datas, Context context) {
+		super();
+		this.datas = datas;
+		this.context = context;
+	}
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return datas.size();
+	}
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return datas.get(position);
+	}
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder viewHolder=null;
+		//≈–∂œ «∑Òª∫¥Ê
+		if(convertView==null){
+			viewHolder=new ViewHolder();
+			convertView=LayoutInflater.from(context).inflate(R.layout.lapin_viewpager_listview_item,null);
+			viewHolder.img=(ImageView) convertView.findViewById(R.id.lapin_listview_item_leftImg);
+			viewHolder.title=(TextView) convertView.findViewById(R.id.lapin_listview_item_title);
+			viewHolder.youhui=(TextView) convertView.findViewById(R.id.lapin_listview_item_youhui);
+			viewHolder.time=(TextView) convertView.findViewById(R.id.lapin_listview_time);
+			convertView.setTag(viewHolder);
+		}else{
+			viewHolder=(ViewHolder) convertView.getTag();
+		}
+		viewHolder.time.setText(datas.get(position));
+		return convertView;
+	}
+	
+	public final class ViewHolder{
+		public ImageView img;
+		public TextView title,youhui,time;
+	}
+	
+
+}
