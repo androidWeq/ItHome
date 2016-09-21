f4d6adaa7ba01deb802341495b3c8df8bca94121
Administrator <1213183178@qq.com>
2016-09-21 09:59:56 +0800

    lsj 9:59

---
+++ src/com/hkd/ithome/activities/Me_Ithome.java
@@ -0,0 +1,16 @@
+package com.hkd.ithome.activities;
+
+
+import com.example.ithome.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Me_Ithome extends Activity{
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.focus_ithome);
+}
+}
---
+++ src/com/hkd/ithome/activities/Me_Login.java
@@ -0,0 +1,17 @@
+package com.hkd.ithome.activities;
+
+import com.example.ithome.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Me_Login extends Activity{
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_me_login);
+	}
+}
