8fc062c715b34311150a8f58f99f7245d3e68261
WEQ <405793463@qq.com>
2016-09-23 15:57:49 +0800

    weq 9/23 15:57

---
+++ src/com/hkd/ithome/activities/LaPinDetial.java
@@ -0,0 +1,17 @@
+package com.hkd.ithome.activities;
+
+
+import com.example.ithome.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LaPinDetial extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.lapin_goods_detial);
+	}
+
+}
