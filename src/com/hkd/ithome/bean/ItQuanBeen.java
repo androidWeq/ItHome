eb0af8f4448d8aadd9713821b77a548c6aae4210
Administrator <1213183178@qq.com>
2016-09-23 15:04:48 +0800

    lsj 15:04

---
+++ src/com/hkd/ithome/bean/ItQuanBeen.java
@@ -0,0 +1,70 @@
+package com.hkd.ithome.bean;
+
+public class ItQuanBeen {
+	private String imgPath;
+	private String title;
+	private String author;
+	private String date;
+	private String author1;
+	private String date1;
+	private int scanner;
+	private int response;
+	private String fromQuan;
+	
+	public String getImgPath() {
+		return imgPath;
+	}
+	public void setImgPath(String imgPath) {
+		this.imgPath = imgPath;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getAuthor1() {
+		return author1;
+	}
+	public void setAuthor1(String author1) {
+		this.author1 = author1;
+	}
+	public String getDate1() {
+		return date1;
+	}
+	public void setDate1(String date1) {
+		this.date1 = date1;
+	}
+	public int getScanner() {
+		return scanner;
+	}
+	public void setScanner(int scanner) {
+		this.scanner = scanner;
+	}
+	public int getResponse() {
+		return response;
+	}
+	public void setResponse(int response) {
+		this.response = response;
+	}
+	public String getFromQuan() {
+		return fromQuan;
+	}
+	public void setFromQuan(String fromQuan) {
+		this.fromQuan = fromQuan;
+	}
+	
+
+}
