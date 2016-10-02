95cd2f1d2968b4d505b687e7bad137c5d5b3dd56
Jack Yabo <392738529@qq.com>
2016-10-02 10:15:16 +0800

    修复资讯页频道滑动闪退的bug

--- src/com/hkd/ithome/tools/NoChange.java
+++ src/com/hkd/ithome/tools/NoChange.java
@@ -5,7 +5,7 @@ public class NoChange {
 	/**
 	 * 服务器地址
 	 */
-	public final static String WEB_SERVERS_ADDRESS="http://192.168.253.6:8080/ItHomeSSH";
+	public final static String WEB_SERVERS_ADDRESS="http://192.168.43.9:8080/ItHomeSSH";
 	
 	/**
 	 * 查询辣品信息的接口
