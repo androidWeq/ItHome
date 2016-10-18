fe20095af2a76f94f7c86d5e40bdccfc981b23fb
Jack Yabo <392738529@qq.com>
2016-10-18 10:06:28 +0800

    更新

--- src/com/hkd/ithome/tools/NoChange.java
+++ src/com/hkd/ithome/tools/NoChange.java
@@ -5,7 +5,7 @@ public class NoChange {
 	/**
 	 * 服务器地址
 	 */
-	public final static String WEB_SERVERS_ADDRESS="http://192.168.1.159:8080/ItHomeSSH";
+	public final static String WEB_SERVERS_ADDRESS="http://192.168.1.102:8080/ItHomeSSH";
 	
 	/**
 	 * 查询辣品信息的接口
