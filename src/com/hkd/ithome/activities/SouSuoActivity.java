69a1e5e1897745f61403ebbf88c1323760764cf9
Administrator <1213183178@qq.com>
2016-10-20 15:50:44 +0800

    lsj 10/20 扫描

--- src/com/hkd/ithome/activities/SouSuoActivity.java
+++ src/com/hkd/ithome/activities/SouSuoActivity.java
@@ -1,5 +1,6 @@
 package com.hkd.ithome.activities;
 
+import com.dtr.zxing.activity.CaptureActivity;
 import com.example.ithome.R;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
@@ -7,13 +8,15 @@ import com.lidroid.xutils.view.annotation.ViewInject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 
-public class SouSuoActivity extends Activity implements OnCheckedChangeListener {
+public class SouSuoActivity extends Activity implements OnCheckedChangeListener,OnClickListener {
 	@ViewInject(R.id.img_sao)
 	ImageView img_sao;// 二维码扫描
 	@ViewInject(R.id.img_soso)
@@ -40,8 +43,7 @@ public class SouSuoActivity extends Activity implements OnCheckedChangeListener 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sosuo_img);
 		ViewUtils.inject(this);
-		rg = (RadioGroup) findViewById(R.id.sosuo_rg);
-		rg.setOnCheckedChangeListener(this);
+		init();
 		Intent intent = getIntent();
 		switch (intent.getIntExtra("name", 0)) {
 		case ZIXUN:// 由资讯页面过来搜索
@@ -57,6 +59,13 @@ public class SouSuoActivity extends Activity implements OnCheckedChangeListener 
 			break;
 		}
 
+	}
+
+	private void init() {
+		// TODO Auto-generated method stub
+		rg = (RadioGroup) findViewById(R.id.sosuo_rg);
+		rg.setOnCheckedChangeListener(this);
+		img_sao.setOnClickListener(this);
 	}
 
 	@Override
@@ -78,6 +87,37 @@ public class SouSuoActivity extends Activity implements OnCheckedChangeListener 
 			break;
 		}
 
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.img_sao:
+			Intent intent = new Intent(SouSuoActivity.this, CaptureActivity.class);
+			startActivityForResult(intent, 0);
+			break;
+
+		default:
+			break;
+		}
+		
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == 12) {
+//			content = data.getExtras().getString("result");
+			String content=data.getStringExtra("context").toString();
+//			tv_show.setText(content);
+			
+//			System.out.println("------------��תWeb");
+			Intent intent2 = new Intent(SouSuoActivity.this,
+					WebviewActivity.class);
+			intent2.putExtra("link",content);
+			startActivity(intent2);
+		}
 	}
 
 }
